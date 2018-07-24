71564f40ecce7d4e44f3b9015dc711878d65bba3
Randall Valentin <randall_valentin@yahoo.com>
2018-07-24 10:46:30 -0400

    Repository Was Created

---
+++ ZVRPub.API/ZVRPub.Repository/ZVRPubRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ZVRPub.Scaffold;
+
+namespace ZVRPub.Repository
+{
+    class ZVRPubRepository
+    {
+
+        private readonly ZVRContext _db;
+
+    }
+}
