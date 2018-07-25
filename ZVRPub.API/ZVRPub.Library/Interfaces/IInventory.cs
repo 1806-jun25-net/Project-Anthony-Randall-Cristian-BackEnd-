ebc8ce8e2af48a73f088060bfe19c5727356be35
Randall Valentin <randall_valentin@yahoo.com>
2018-07-25 11:47:44 -0400

    Started Some Interfaces

---
+++ ZVRPub.API/ZVRPub.Library/Interfaces/IInventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZVRPub.Library.Interfaces
+{
+    interface IInventory
+    {
+         int Id { get; set; }
+         string IngredientName { get; set; }
+         string IngredientType { get; set; }
+         decimal Price { get; set; }
+
+    }
+}
