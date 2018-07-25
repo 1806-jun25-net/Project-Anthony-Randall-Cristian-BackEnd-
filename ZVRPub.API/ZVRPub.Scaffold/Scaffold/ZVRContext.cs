bcd4d4d1adc4a0a7cb5e22adc359d2d9dc255bc4
Randall Valentin <randall_valentin@yahoo.com>
2018-07-25 10:22:09 -0400

    Deliting connection string from my branch . Sorry

--- ZVRPub.API/ZVRPub.Scaffold/Scaffold/ZVRContext.cs
+++ ZVRPub.API/ZVRPub.Scaffold/Scaffold/ZVRContext.cs
@@ -30,11 +30,6 @@ namespace ZVRPub.Scaffold
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:valentin-quinones-1806.database.windows.net,1433;Initial Catalog=ZVR;Persist Security Info=False;User ID=NinjaUltraStar;Password=Number39@#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
