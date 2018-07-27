641ed9b302804325010eb269a6c678da3d987d83
ajzoell <ajzoellner@gmail.com>
2018-07-27 11:38:19 -0400

    updated nuget packages and began implementing unit testing on user controller

---
+++ ZVRPub.API/XUnitTestZVRPub.API/UserControllerTesting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestZVRPub.API
+{
+    public class UserControllerTesting
+    {
+        [Fact]
+        public async Task UserIndexAsyncShouldReturnAllUsers()
+        {
+        }
+        
+    }
+}
+}
