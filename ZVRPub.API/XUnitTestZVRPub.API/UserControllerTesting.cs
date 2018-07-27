fad87e670ba7e9d9a3d3cd583d2a3c8ee5e224e6
ajzoell <ajzoellner@gmail.com>
2018-07-27 11:41:53 -0400

    still working on unit testing

--- ZVRPub.API/XUnitTestZVRPub.API/UserControllerTesting.cs
+++ ZVRPub.API/XUnitTestZVRPub.API/UserControllerTesting.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using ZVRPub.Scaffold;
 
 namespace XUnitTestZVRPub.API
 {
     public class UserControllerTesting
     {
         [Fact]
-        public async Task UserIndexAsyncShouldReturnAllUsers()
+        public async Task UserControllerGetAllMethodShouldReturnAllUsers()
         {
         }
         
+        private List<Users> GetTestUsers()
+        {
+            var users = new List<Users>();
+            users.Add(new Users)
+        }
     }
-}
 }
