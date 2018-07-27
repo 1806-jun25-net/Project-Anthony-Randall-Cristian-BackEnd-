d5675012b7b8fcc1700cb51faab20700ab3eda76
ajzoell <ajzoellner@gmail.com>
2018-07-27 16:12:35 -0400

    working on inventory controller tests

---
+++ ZVRPub.API/XUnitTestZVRPub.API/InventoryControllerTesting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ZVRPub.Scaffold;
+
+namespace XUnitTestZVRPub.API
+{
+    public class InventoryControllerTesting
+    {
+        [Fact]
+        public void InventoryControllerShouldBeAbleToReturnAllLocationInventories()
+        {
+            InventoryHasLocation inventory1 = new InventoryHasLocation
+            {
+                
+            };
+            var inventory2 = new InventoryHasLocation
+            {
+                
+            };
+
+            var repoMock = new Mock<IZVRPubRepository>();
+            repoMock.Setup(c => c.GetUsers()).Returns(new List<Users>
+            {
+               user1, user2
+            });
+
+            var controller = new UserController(repoMock.Object);
+            var result = controller.GetAll();
+
+            Assert.NotNull(result.Value);
+
+            Assert.Same(user1, result.Value[0]);
+            Assert.Same(user2, result.Value[1]);
+        }
+    }
+}
