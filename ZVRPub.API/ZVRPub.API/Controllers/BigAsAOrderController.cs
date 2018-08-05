4e390366d6f23581d20dfa800c6180afe1032b8e
Cristian J. Rosario <cris0u0rosario@gmail.com>
2018-08-05 16:42:19 -0400

    hi nick whats up you like my code? comiit yes  if you like if you dont like my code then dont commit at all

--- ZVRPub.API/ZVRPub.API/Controllers/BigAsAOrderController.cs
+++ ZVRPub.API/ZVRPub.API/Controllers/BigAsAOrderController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NLog;
 using ZVRPub.API.ModelsNeeded;
+using ZVRPub.Library.Model;
 using ZVRPub.Repository;
 using ZVRPub.Scaffold;
 
@@ -34,65 +35,31 @@ namespace ZVRPub.API.Controllers
             
         }
         [HttpGet]
-        public ActionResult<List<UserHasPreBuildMenuAndInvModel>> Get()
+        public ActionResult<List<SuperOrder>> Get()
         {
             var user = Repo.GetUsers();
             var inv = Repo.GetInventories().ToList();
             var loc = Repo.GetLocations().ToList();
 
             var orderList = Repo.GetOrders();
-            
-          
 
-            var preBuiltList = Repo.GetMenuPreBuiltHasOrders();
-            var getPreOrder = new List<MenuPrebuiltHasOrders>();
-            foreach (var item in orderList)
+            var model = new List<SuperOrder>();
+
+
+            foreach(var item in orderList)
             {
-                foreach (var item2 in preBuiltList)
+                model.Add(new SuperOrder
                 {
-                    if (item.OrderId == item2.OrdersId)
-                    {
-                        getPreOrder.Add(item2);
-                    }
+                    orderTime = item.OrderTime,
+                    cost = item.Cost,
+                    user = Repo.GetUserByUserById(item.UserId).Username, 
+                    location = Repo.GetLocationById(item.LocationId).City
 
-                }
+                    
+                });
             }
 
-
-
-
-
-            var getPreOrderMenu = Repo.GetAllMenuPreBuilt().ToList();
-
-
-            IEnumerable<LocationModel> location2 = loc.Select(x => new LocationModel
-            { Id = x.Id, City = x.City });
-            IEnumerable<InventoryModel> inventory2 = inv.Select(x => new InventoryModel
-            { Id = x.Id, IngredientName = x.IngredientName, Price = x.Price });
-            IEnumerable<OrderModel> order2 = orderList.Select(x => new OrderModel
-            { OrderId = x.OrderId, UserId = x.UserId, LocationId = x.LocationId, OrderTime = x.OrderTime });
-            IEnumerable<MenuPrebuiltHasOrdersModel> PreOr = getPreOrder.Select(x => new MenuPrebuiltHasOrdersModel
-            { Id = x.Id, MenuPreBuildId = x.MenuPreBuildId, OrdersId = x.OrdersId });
-            IEnumerable<MenuPreBuiltModel> PreBuilt = getPreOrderMenu.Select(x => new MenuPreBuiltModel
-            { Id = x.Id, NameOfMenu = x.NameOfMenu, Price = x.Price, TwentyOneOver = x.TwentyOneOver });
-
-            List<UserHasPreBuildMenuAndInvModel> model = new List<UserHasPreBuildMenuAndInvModel>();
-            foreach(var item in user)
-            {
-                model.Add(new UserHasPreBuildMenuAndInvModel
-                {
-
-                    User = item.Username,
-                    Location = location2,
-                    Inventories = inventory2,
-                    Order = order2,
-                    PreBuilt = PreBuilt,
-                    PreBuiltHasOrder = PreOr
-
-
-                }
-              );    
-            }
+         
            
 
             return model;
